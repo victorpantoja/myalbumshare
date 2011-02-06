5f4e3ae96f2752660c0bd4b7215c334a6ce8c657
Victor Pantoja <victor.pantoja@corp.globo.com>
2011-02-06 17:31:14 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myalbumshare::Application.config.secret_token = '6793d83d77a11384b415d10cd36c5906883cf2d601193196442d44ba565560b3d03aca93540dd80b49fa3d9983946d8673edfa66679bf2c2baf27ef17ab16ba8'
